8ab9bda155eb98d313658af1ec055985b58f6550
Nidhi Bhatia <nidhibhatia7078@gmail.com>
2020-05-03 00:04:03 +0530

    add api

--- frontend/nbproject/src/store/service/getService.js
+++ frontend/nbproject/src/store/service/getService.js
@@ -1,21 +1,22 @@
 import axios from "axios";
 
 export const getDataService = (req) => {
-  console.log("req", req);
+  console.log("req", req.data);
   let headers = new Headers();
   headers.append("Content-Type", "application/json");
   headers.append("Accept", "application/json");
   headers.append("Origin", "http://localhost:3001");
+  const params = new URLSearchParams();
+  params.append("name", req.name);
+  params.append("email", req.email);
 
   return axios({
-    method: "GET",
-    url:
-      "https://infinite-wildwood-05252.herokuapp.com/NBProject/adminExtractAllData",
-
+    method: "POST",
+    url: " https://infinite-wildwood-05252.herokuapp.com/NBProject/adminSignup",
+    data: req,
     headers: headers,
   })
     .then((response) => {
-      console.log("response", response);
       return response;
     })
     .catch((err) => {
